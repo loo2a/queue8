8225a6ed762db61463326f92ae7cdbb3996057e0
loo2a <lu2adeveloper@gmail.com>
2025-11-22 20:18:51 +0200

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -6,14 +6,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getDatabase, ref, set, onValue, push, remove } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA3SLxFOgHihiZ-PPt-_BWWRYa3wMNDPXs",
+  authDomain: "queue8-e4ba6.firebaseapp.com",
+  databaseURL: "https://queue8-e4ba6-default-rtdb.firebaseio.com",
+  projectId: "queue8-e4ba6",
+  storageBucket: "queue8-e4ba6.firebasestorage.app",
+  messagingSenderId: "957739657206",
+  appId: "1:957739657206:web:33d52aea84d9f6b920c9d7",
+  measurementId: "G-6W02DB5GNC"
 };
 
 // Initialize Firebase
